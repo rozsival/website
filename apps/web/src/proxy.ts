35d021e80cc2921d52f4ea61c25a214e1afd1923
Vít Rozsíval <mail@vitrozsival.cz>
2026-01-24 19:57:25 +0100

    fix(proxy): matcher

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -78,5 +78,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
 };
